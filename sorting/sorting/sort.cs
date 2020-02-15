cd0aeb6e0a96f3dea5bd392649125ef502e24d37
godinseccoming@gmail.com <godinseccoming@gmail.com>
2020-02-16 00:24:17 +0300

    realese bubble, quick and merge sorting

--- sorting/sorting/sort.cs
+++ sorting/sorting/sort.cs
@@ -74,6 +74,11 @@ namespace sorting
 
             return sorted;
         }
+        /// <summary>
+        /// This method sorting array of integer value by insertion sorting
+        /// </summary>
+        /// <param name="toSort">Array to sort</param>
+        /// <returns>Sorted array</returns>
         public int[] insertionSort(int[] toSort)
         {
             //Сортировка вставками
@@ -86,7 +91,7 @@ namespace sorting
             {
                 int key = sorted[i];    //Берём значение начиная со второго элемента в массиве
                 int j = i--;            //Берём индекс предыдущего
-                while(j>=0 && sorted[j]>key) //Пока не проверим весь отрезок от начала до sorted[i]
+                while (j >= 0 && sorted[j] > key) //Пока не проверим весь отрезок от начала до sorted[i]
                 {
                     sorted[j + 1] = sorted[j]; //Если предыдущий элемент больше, смещаем его правее
                     j--;                       //Идём к следующему (смещаемся влево)
@@ -96,5 +101,166 @@ namespace sorting
 
             return sorted;
         }
+        /// <summary>
+        /// This method sorting array of integer value by bubble sorting
+        /// </summary>
+        /// <param name="toSort">Array to sort</param>
+        /// <returns>Sorted array</returns>
+        public int[] bubbleSort(int[] toSort)
+        {
+            //Сортировка пузырьком
+
+            int[] sorted = new int[toSort.Length];
+            Array.Copy(toSort, sorted, toSort.Length);//Создаём копию исходного массива
+
+            for (int i = 0; i < sorted.Length; i++) 
+            {
+                for (int j = sorted.Length - 2; j >= i; j--)  //Идём справа на лево по массиву и сравниваем в цикле два рядом стоящих значения
+                {
+                    if (sorted[j] > sorted[j + 1])
+                    {
+                        int temp = sorted[j];
+                        sorted[j] = sorted[j + 1];
+                        sorted[j + 1] = temp; 
+                    }
+                }
+            }
+
+            return sorted;
+        }
+        //-----------------------------------------------------Метод слияния--------------------------------------------------------------------------------
+        /// <summary>
+        /// This method sorting array of integer value by merge sorting
+        /// </summary>
+        /// <param name="toSort">Array to sort</param>
+        /// <returns>Sorted array</returns>
+        public int[] mergeSort(int[] toSort)
+        {
+            //Сортировка методом слияния
+
+            int[] sorted = new int[toSort.Length];
+            Array.Copy(toSort, sorted, toSort.Length);//Создаём копию исходного массива
+
+
+            return MergeSort(sorted, 0, sorted.Length - 1);
+        }
+
+        static void Merge(int[] array, int lowIndex, int middleIndex, int highIndex)
+        {
+            var left = lowIndex;
+            var right = middleIndex + 1;
+            var tempArray = new int[highIndex - lowIndex + 1];
+            var index = 0;
+
+            while ((left <= middleIndex) && (right <= highIndex))
+            {
+                if (array[left] < array[right])
+                {
+                    tempArray[index] = array[left];
+                    left++;
+                }
+                else
+                {
+                    tempArray[index] = array[right];
+                    right++;
+                }
+
+                index++;
+            }
+
+            for (var i = left; i <= middleIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+
+            for (var i = right; i <= highIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+
+            for (var i = 0; i < tempArray.Length; i++)
+            {
+                array[lowIndex + i] = tempArray[i];
+            }
+
+        }
+        //сортировка слиянием
+        static int[] MergeSort(int[] array, int lowIndex, int highIndex)
+        {
+            if (lowIndex < highIndex)
+            {
+                var middleIndex = (lowIndex + highIndex) / 2;
+                MergeSort(array, lowIndex, middleIndex);
+                MergeSort(array, middleIndex + 1, highIndex);
+                Merge(array, lowIndex, middleIndex, highIndex);
+            }
+
+            return array;
+        }
+        //-----------------------------------------------------Метод слияния--------------------------------------------------------------------------------
+
+
+        //-----------------------------------------------------Метод быстрой сортировки---------------------------------------------------------------------
+
+        /// <summary>
+        /// This method sorting array of integer value by quick sorting
+        /// </summary>
+        /// <param name="toSort">Array to sort</param>
+        /// <returns>Sorted array</returns>
+        public int[] quickSort(int[] toSort)
+        {
+            int[] sorted = new int[toSort.Length];
+            Array.Copy(toSort, sorted, toSort.Length);//Создаём копию исходного массива
+
+
+            return QuickSort(sorted, 0, sorted.Length - 1);
+
+        }
+
+        //метод для обмена элементов массива
+        static void swap(ref int x, ref int y)
+        {
+            var t = x;
+            x = y;
+            y = t;
+        }
+
+        //метод возвращающий индекс опорного элемента
+        static int Partition(int[] array, int minIndex, int maxIndex)
+        {
+            var pivot = minIndex - 1;
+            for (var i = minIndex; i < maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    swap(ref array[pivot], ref array[i]);
+                }
+            }
+
+            pivot++;
+            swap(ref array[pivot], ref array[maxIndex]);
+            return pivot;
+        }
+
+        //быстрая сортировка
+        static int[] QuickSort(int[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+
+            var pivotIndex = Partition(array, minIndex, maxIndex);
+            QuickSort(array, minIndex, pivotIndex - 1);
+            QuickSort(array, pivotIndex + 1, maxIndex);
+
+            return array;
+        }
+
+        //-----------------------------------------------------Метод быстрой сортировки---------------------------------------------------------------------
+
     }
 }
